40ee545093d76a29804fc3816aeedff61f2e1a23
Quantum <aburorhambagadaniel@gmail.com>
2026-03-12 02:14:14 +0100

    edited contact component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import AboutSection from '../components/AboutSection';
 import CoursesSection from '../components/CoursesSection';
 import WhyChooseUs from '../components/WhyChooseUs';
 import Testimonials from '../components/Testimonials';
-import ContactNewsletter from '../components/ContactNewsletter';
+import ContactNewsletter from '../components/ContactNewsLetter';
 import '../App.css';
 
 
